b782414c26f86632bc1c8012485d97179951a375
Agathe Diouf <agathegnilanediouf@gmail.com>
2023-01-25 22:06:42 +0100

    text updated at home page(2)

--- components/home/SectionAbout.jsx
+++ components/home/SectionAbout.jsx
@@ -9,7 +9,7 @@ export default function SectionPosition() {
           Currently, I am an Temporary Research and Teaching Attached (ATER)  in Statistics in Laboratoire de Matématique at Bretagne Atlantique (LMBA, UMR CNRS 6205) at Université Bretagne Sud (UBS, Campus of Vannes), since September 2022.  Before, I was a Statistics postdoctoral researcher at Laboratoire de Mathématiques Raphael Salem at Rouen University, France. I received a Ph.D. in Statistic applied from Gaston Berger University (UGB) of Saint-Louis, Senegal in july 2021, a thesis directed by Aliou Diop ( UGB, Saint-Louis) and Jean-François Dupuy ( INSA of Rennes, France)
         </p>
         <p className='pt-5 sm:leading-relaxed lg:leading-relaxed md:leading-loose md:text-2xl lg:text-base'>
-          My research areas are statistical learning and applied mathematics to health care. Primarily, my research is motivated by problems of Zero inflated (ZI) in count data commonly encountered in many fields such as health economics, environment and insurance. My current favorite research is on statistical inference (mathematical study: asymptotic properties in these models).  I am interested in both theoretical results and practical methods and their application to count data.
+        My research interests are motivated by the Zero Inflated (ZI) problems in count data commonly encountered in many fields such as health economics, environment and insurance. My current favorite research focuses on statistical inference (mathematical study: asymptotic properties in these models). I am interested in both theoretical results and practical methods and their application to count data.
         </p>
     </div>
   )
