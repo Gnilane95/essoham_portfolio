81065df302dd254c8db7e0309c8f6ff075907d6c
Agathe Diouf <agathegnilanediouf@gmail.com>
2023-01-25 22:00:44 +0100

    text updated at home page

--- pages/publications.js
+++ pages/publications.js
@@ -7,32 +7,44 @@ import { NextSeo } from 'next-seo';
 export default function publications() {
   const publications =[
     {
+      title : 'A simulation-based study of Zero-inflated Bernoulli model with various models for the susceptible probability.',
+      journal : 'In progress',
+      authors :'Essoham Ali, Kim-Hung Pho',
+      year : '2022',
+      to : 'https://arxiv.org/abs/2301.09698',
+      id : 1,
+    },
+    {
       title : 'Statistical inference in a Zero-Inflated Bell regression model.',
       journal : 'Mathematical Methods of Statistics',
       authors :'Essoham Ali, Mamadou Lamine Diop, Aliou Diop',
       year : '2022',
-      id : 1,
+      to : 'https://link.springer.com/article/10.3103/S1066530722030012',
+      id : 2,
     },
     {
       title : 'A simulation-based study of ZIP regression with various zero-inflated submodels.',
       journal : 'Communications in Statistics - Simulation and Computation',
       authors :'Essoham Ali',
       year : '2022',
-      id : 2,
+      to : 'https://www.tandfonline.com/doi/abs/10.1080/03610918.2022.2025840',
+      id : 3,
     },
     {
       title : 'A constrained marginal zero-inflated binomial regression.',
       journal : 'Communications in Statistics - Theory and Methods',
       authors :'Ali E., Diop A., Dupuy J.-F',
       year : '2022',
-      id : 3,
+      to : 'https://www.tandfonline.com/doi/abs/10.1080/03610926.2020.1861296',
+      id : 4,
     },
     {
       title : '16S metagenomic comparison of Plasmo-dium falciparum infected and non-infected Anopheles gambiae and Anopheles funestus Microbiota from Senegal',
       journal : 'American Journal of Tropical Medicine Hygiene',
       authors :'Hubert Bassene, El Hadji Amadou Niang, Florence Fenollar, Dipankar Bachar, Souleymane Doucouré, Essoham Ali, Caroline Michelle, Didier Raoult, Cheickh Sokhna and Oleg Mediannikov',
       year : '2018',
-      id : 4,
+      to : 'https://pubmed.ncbi.nlm.nih.gov/30350766/',
+      id : 5,
     }
   ]
   return (
@@ -47,13 +59,6 @@ export default function publications() {
           Publications
         </h1>
         <div className='lg:mx-28 xl:mx-44 sm:mx-10 md:mx-24'>
-          <div className='shadow-lg p-7 mb-16'>
-            <h2 className='sm:text-xl md:text-2xl lg:text-xl'>
-              A simulation-based study of Zero-inflated Bernoulli model with various models for the susceptible probability
-            </h2>
-            <p className='sm:text-base md:text-xl lg:text-base font-bold text-amber-800 pb-5'>In progress</p>
-            <p className='sm:text-base md:text-xl lg:text-base'>Essoham Ali, Kim-Hung Pho | 2022</p>
-          </div>
           {publications.map((publication) =>(
             <div className='shadow-lg p-7 mb-16' key={publication.id}>
               <h2 className='sm:text-xl md:text-2xl lg:text-xl'>
@@ -66,7 +71,7 @@ export default function publications() {
                 <p className='sm:text-base md:text-xl lg:text-base'>
                   {publication.authors} | {publication.year}
                 </p>
-                <Link href='https://link.springer.com/article/10.3103/S1066530722030012' className='btn sm:btn-sm lg:btn-sm md:btn-md border-none bg-amber-700 sm:mt-5 lg:mt-0 px-5 hover:bg-slate-100 hover:text-gray-800'>
+                <Link href={publication.to} className='btn sm:btn-sm lg:btn-sm md:btn-md border-none bg-amber-700 sm:mt-5 lg:mt-0 px-5 hover:bg-slate-100 hover:text-gray-800'>
                   Click to see
                 </Link>
               </div>
