33557dd822a4f0c4b87861cfd83c530c0cd47b22
Kudayah Sassou Horacio Herve <36052706+horacioskrp@users.noreply.github.com>
2023-12-10 12:22:06 +0000

    Update CustomHead.jsx

--- components/layout/CustomHead.jsx
+++ components/layout/CustomHead.jsx
@@ -4,7 +4,7 @@ import Head from 'next/head';
 
 const CustomHead = () => (
   <Head>
-    <meta name="google-site-verification" content="sjC9Cx6bvOd54NSxQQPxmWhlW4NY0CzKox8JWHe6d_g" />
+    <meta name="google-site-verification" content="F-6apCq-q6spy6kcgKkVaJynPcMkC1ySor63mRwZoDA" />
   </Head>
 );
 
