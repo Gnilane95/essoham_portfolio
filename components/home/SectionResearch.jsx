4a815539bb5523c9262a977cd0f27edb00d54a59
Agathe Diouf <agathegnilanediouf@gmail.com>
2025-04-09 16:52:30 +0200

    Replace Research interests text

--- components/home/SectionResearch.jsx
+++ components/home/SectionResearch.jsx
@@ -7,10 +7,11 @@ export default function SectionResearch() {
       <div className="text-justify pb-10">
         <SiteHeader title={"Research interests"}/>
         <p className="sm:leading-relaxed lg:leading-relaxed md:leading-loose md:text-base pb-2">
-         My main research focus lies at the intersection of theory and application of zero-inflated count models, missing data, and statistical learning. I am particularly interested in the challenges related to count data, especially when these data exhibit zero-inflation and multicollinearity phenomena, as well as handling missing data in these contexts.
+        My research primarily focuses on the interface between theory and application in the modeling of zero-inflated count data, missing data mechanisms, and statistical learning. I am particularly interested in the methodological and computational challenges posed by count data, especially in the presence of zero inflation, multicollinearity, and missingness.
+
         </p>
         <p className="sm:leading-relaxed lg:leading-relaxed md:leading-loose md:text-base pb-2">
-          More specifically, I work on developing and analyzing the properties of methodologies to solve estimation problems in count models with potential nonresponse, using tools from statistical learning. I am also interested in situations where the number of covariates is particularly large (high-dimensional scenarios), which further complicates the analysis of the data.
+        More specifically, my work involves developing and studying the theoretical properties of estimation techniques tailored to zero-inflated count models affected by nonresponse, drawing on tools from statistical learning and regularization. I also explore high-dimensional settings where the number of covariates is large, further complicating inference and variable selection. My ultimate goal is to contribute robust and interpretable solutions that are applicable to real-world problems, particularly in health economics, environmental science, and social data.
         </p>
       </div>
     </>
